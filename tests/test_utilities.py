fa87c86cf3fb1fa39a1b1ea9d45298ba09970f0a
Shivam Mehta <shivam.mehta007@gmail.com>
2022-09-26 14:32:08 +0200

    Testing for invertibility with random initialization

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -2,6 +2,7 @@ import os
 import random
 
 import torch
+import torch.nn as nn
 
 from tests import PACKAGE_ROOT
 
@@ -22,3 +23,22 @@ def get_a_mel():
 
 def get_a_text_mel_pair():
     return get_a_text(), get_a_mel()
+
+
+def reset_all_weights(model: nn.Module) -> None:
+    """
+    refs:
+        - https://discuss.pytorch.org/t/how-to-re-set-alll-parameters-in-a-network/20819/6
+        - https://stackoverflow.com/questions/63627997/reset-parameters-of-a-neural-network-in-pytorch
+        - https://pytorch.org/docs/stable/generated/torch.nn.Module.html
+    """
+
+    @torch.no_grad()
+    def weight_reset(m: nn.Module):
+        # - check if the current module has reset_parameters & if it's callabed called it on m
+        reset_parameters = getattr(m, "reset_parameters", None)
+        if callable(reset_parameters):
+            m.reset_parameters()
+
+    # Applies fn recursively to every submodule see: https://pytorch.org/docs/stable/generated/torch.nn.Module.html
+    model.apply(fn=weight_reset)

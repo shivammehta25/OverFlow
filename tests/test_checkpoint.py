86708a7e3f37bbdffea67de9c6078fcd10e2c926
Shivam Mehta <shivam.mehta007@gmail.com>
2022-11-14 11:57:54 +0000

    Fixing the checkpoint loading test

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -4,9 +4,7 @@ import pytorch_lightning as pl
 from src.training_module import TrainingModule
 
 
-# TODO: remove the skip
-@pytest.mark.skip(reason="Will save a checkpoint first then run it")
-@pytest.mark.parametrize("checkpoint_path", ["checkpoint_326000.ckpt"])
+@pytest.mark.parametrize("checkpoint_path", ["OverFlow-Male.ckpt", "OverFlow-Female.ckpt"])
 def test_loading_checkpoint(checkpoint_path):
     model = TrainingModule.load_from_checkpoint(checkpoint_path)
     assert isinstance(model, pl.LightningModule)
